abbd5d0a6ba371cc01f6d6ba95f60c42f1c556b5
sergesoroka <sergesoroka@gmail.com>
2022-03-07 16:52:16 +0200

    Sidebar open/close lists

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,40 +1,59 @@
+import { useState } from 'react'
 import styles from './Sidebar.module.css'
 
 export default function Sidebar() {
+
+const [diary, setDiary] = useState(true)
+const [whiteList, setWhiteList] = useState(false)
+const [blackList, setBlackList] = useState(false)
+const [naratives, setNaratives] = useState(false)
+
   return (
     <div className={styles.sidebar}>
         <div className={styles.sidebarItem}>
-            <h3 className={styles.sidebarTitles}># Diary</h3>
+            <h3 className={diary ? styles.sidebarTitlesActive : styles.sidebarTitles}
+            onClick={() => setDiary(!diary)}
+            ># Diary</h3>
+            {diary ?
             <ul className={styles.list}>
                 <li className={styles.listItem}>Antispiegel</li>
                 <li className={styles.listItem}>SNA.DE</li>
                 <li className={styles.listItem}>Report24 News</li>
-            </ul>
-        </div>
+            </ul> : null }
+        </div>  
         <div className={styles.sidebarItem}>
-            <h3 className={styles.sidebarTitles}># White List</h3>
+            <h3 className={whiteList ? styles.sidebarTitlesActive : styles.sidebarTitles}
+                onClick={() => setWhiteList(!whiteList)}
+                ># White List</h3>
+                {whiteList ?
             <ul className={styles.list}>
                 <li className={styles.listItem}>BBC</li>
                 <li className={styles.listItem}>New Yorker</li>
                 <li className={styles.listItem}>24 News</li>
-            </ul>
+            </ul> : null }
         </div>
         <div className={styles.sidebarItem}>
-            <h3 className={styles.sidebarTitles}># Black List</h3>
+            <h3 className={blackList ? styles.sidebarTitlesActive : styles.sidebarTitles}
+                onClick={() => setBlackList(!blackList)}
+                ># Black List</h3>
+            {blackList ? 
             <ul className={styles.list}>
                 <li className={styles.listItem}>RT</li>
-            </ul>
+            </ul> : null }
         </div>
         <div className={styles.sidebarItem}>
-            <h3 className={styles.sidebarTitles}># Naratives</h3>
+            <h3 className={naratives ? styles.sidebarTitlesActive : styles.sidebarTitles}
+                onClick={() => setNaratives(!naratives)}
+                ># Naratives</h3>
+            {naratives ?
             <ul className={styles.list}>
                 <li className={styles.listItem}>
                     Maidan in 2014 was a coup d'état
                 </li>
                 <li className={styles.listItem}>
                 Ukraine fails to deliver commitments under Minsk Agreements/Kyiv does not comply with the Minsk agreements
-                </li>
-            </ul>
+                </li> 
+            </ul> : null }
         </div>
     </div>
   )

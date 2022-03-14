dfc6810b1e1da23b1a2670f1668321d65efba41b
sergesoroka <sergesoroka@gmail.com>
2022-03-14 08:57:15 +0200

    fake list edited

--- components/FakeList/FakeItem/FakeItem.jsx
+++ components/FakeList/FakeItem/FakeItem.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import styles from './FakeItem.module.css'
+import React, { useState } from "react";
+import styles from "./FakeItem.module.css";
 
-export default function FakeItem({data, narrative}) {
+export default function FakeItem({ data, narrative }) {
+  const [toggle, setToggle] = useState(false);
+  const toggleHandler = (e) => {
+    console.log(e);
+  };
 
-//    const renderedData = data.map((item, i) => {
-//     return (
-//         <div key={item.Date} >
-//         <div className={styles.fakeItemHedingWrap}>
-//             {/* { item.Narrative.includes(item.Narrative) ?
-//             <h2 className={styles.fakeHeading}>{item.Narrative}</h2>
-//             : null } */}
-//         </div>
-//         <table><tbody>
-//             <tr className={styles.fakeItemWrap}>
-//                 <td className={styles.fakeNote}>{item.Disinfo_cases_en}</td>
-//                 <td className={styles.fakeSource}>{item.Media}</td>
-//                 <td className={styles.fakeDate}>{item.Date}</td>
-//             </tr>
-//         </tbody></table>
-//         </div>
-//        )
-//    })
+  const renderedData = data.map((item, i) => {
+    if (item.Narrative === narrative) {
+      return (
+        <div key={i}>
+          <div className={styles.fakeItemHedingWrap}></div>
+          <table>
+            <tbody>
+              <tr className={styles.fakeItemWrap}>
+                <td className={styles.fakeNote}>{item.Disinfo_cases_en}</td>
+                <td className={styles.fakeSource}>{item.Media}</td>
+                <td className={styles.fakeDate}>{item.Date}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  });
+
   return (
     <div>
-       {narrative ? <h2 key={narrative} className={styles.fakeHeading}>#  {narrative}</h2> : null}
-       
+      {narrative ? (
+        <h2
+          key={narrative}
+          className={styles.fakeHeading}
+          onClick={(e) => toggleHandler(e)}
+        >
+          # {narrative}
+        </h2>
+      ) : null}
+      {renderedData}
     </div>
-  )
+  );
 }
--- components/FakeList/FakeList.jsx
+++ components/FakeList/FakeList.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import FakeItem from './FakeItem/FakeItem'
-import styles from './FakeList.module.css'
+import React from "react";
+import FakeItem from "./FakeItem/FakeItem";
+import styles from "./FakeList.module.css";
 
-import { fakeData } from '../../data/data'
+import { fakeData } from "../../data/data";
 
 export default function FakeList() {
   const uniqueNarratives = [];
-  fakeData.forEach((c) => {
-        if (!uniqueNarratives.includes(c.Narrative)) {
-            uniqueNarratives.push(c.Narrative);
-        }
-    });
-  const narrative = uniqueNarratives.map(n => <FakeItem key={n} data={fakeData} narrative={n}/>)
-  return (
-      <div className={styles.fakeListWrap}>
-        {narrative}
-      </div>
-  )
+  const dataForNarrative = fakeData.map((c) => {
+    if (!uniqueNarratives.includes(c.Narrative)) {
+      uniqueNarratives.push(c.Narrative);
+    }
+    return c;
+  });
+  
+  const narrative = uniqueNarratives.map((n) => (
+    <FakeItem key={n} data={fakeData} narrative={n} />
+  ));
+  return <div className={styles.fakeListWrap}>{narrative}</div>;
 }

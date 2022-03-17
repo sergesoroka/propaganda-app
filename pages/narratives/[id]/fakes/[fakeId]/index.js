84240ec71797d51475e8d06ddcb4905468b60714
sergesoroka <sergesoroka@gmail.com>
2022-03-17 15:37:20 +0200

    Fake page details

--- pages/narratives/[id]/fakes/[fakeId]/index.js
+++ pages/narratives/[id]/fakes/[fakeId]/index.js
@@ -11,7 +11,17 @@ export default function FakeItem() {
         <BsArrowLeft />
       </div>
       <h3 className={styles.fakeItemHeading}># {id}</h3>
+      <div style={{ display: "flex" }}>
+        <div className={styles.country}>Німеччина</div>
+        <div className={styles.media}>
+          <a href="https://uncutnews.ch/die-nato-rueckt-in-der-ukraine-immer-naeher-an-einen-krieg-gegen-russland/">
+            Uncutnews
+          </a>
+        </div>
+        <div className={styles.date}>01/11/2021</div>
+      </div>
       <div className={styles.debunking}>
+        <h4 className={styles.debunkingHeading}>Debunking</h4>
         In 2021, The American George Washington Institute
         published(https://www.illiberalism.org/far-right-group-made-its-home-in-ukraines-major-western-military-training-hub/)
         a report stating that the far-right group Centuria has been using the
@@ -22,10 +32,11 @@ export default function FakeItem() {
         infiltrated the Ukrainian government. There are dozens of organizations
         that are not just far-right, but openly pro-Nazi, and their merging with
         law enforcement agencies, state authorities and local self-government
-        has become an open secret wrote(https://klymenko-time.com/novosti/naczizm-s-gosfinansirovaniem-kak-podderzhivaemaya-ssha-voennaya-akademiya-stala-domom-dlya-ultrapravoj-organizaczii-centuria-yutk/?utm_source=telegram&utm_medium=smm&utm_campaign=novosti_ktime_telegram)
+        has become an open secret
+        wrote(https://klymenko-time.com/novosti/naczizm-s-gosfinansirovaniem-kak-podderzhivaemaya-ssha-voennaya-akademiya-stala-domom-dlya-ultrapravoj-organizaczii-centuria-yutk/?utm_source=telegram&utm_medium=smm&utm_campaign=novosti_ktime_telegram)
         Klymenko Time. The First Independent, on the other hand, spoke of Nazis
-        roaming the streets of Ukraine. Let&quot;s address the elephant in the room.
-        Ukraine, like the overwhelming majority of developed countries,
+        roaming the streets of Ukraine. Let&quot;s address the elephant in the
+        room. Ukraine, like the overwhelming majority of developed countries,
         including Russia, has a number of organizations belonging to the far
         right of the political spectrum, but these organizations are few and do
         not enjoy the levels of support in society and government the

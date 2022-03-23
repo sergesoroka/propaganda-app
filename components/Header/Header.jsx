a745d490e747faa81720ec7f147d00a99e0b32a4
Serge <sergesoroka@gmail.com>
2022-03-23 12:11:41 +0200

    Added Lang Pages and Links Styles

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,24 +1,58 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./Header.module.css";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <div className={styles.headerWrap}>
       <div className={styles.langMenu}>
         <ul className={styles.mainMenuList}>
-          <li className={styles.mainMenuListItem}>
+          <li
+            className={
+              router.pathname == "/lang/it"
+                ? styles.activeLink
+                : styles.mainMenuListItem
+            }
+          >
             <Link href="/lang/it">
               <a>IT</a>
             </Link>
           </li>
-          <li className={styles.mainMenuListItem}>
+          <li
+            className={
+              router.pathname == "/lang/de"
+                ? styles.activeLink
+                : styles.mainMenuListItem
+            }
+          >
             <Link href="/lang/de">
               <a>DE</a>
             </Link>
           </li>
-          <li className={styles.mainMenuListItem}>ENG</li>
-          <li className={styles.mainMenuListItem}>RU</li>
+          <li
+            className={
+              router.pathname == "/lang/en"
+                ? styles.activeLink
+                : styles.mainMenuListItem
+            }
+          >
+            <Link href="/lang/en">
+              <a>ENG</a>
+            </Link>
+          </li>
+          <li
+            className={
+              router.pathname == "/lang/ru"
+                ? styles.activeLink
+                : styles.mainMenuListItem
+            }
+          >
+            <Link href="/lang/ru">
+              <a>RU</a>
+            </Link>
+          </li>
         </ul>
       </div>
       <div className={styles.mainMenu}>

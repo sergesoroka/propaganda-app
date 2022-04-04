a9962634c6856d4d08d9a6d0b27dce3dabb53bee
Serge <sergesoroka@gmail.com>
2022-04-04 18:13:38 +0300

    Link Blank Fixed

--- components/MediaList/MediaList.jsx
+++ components/MediaList/MediaList.jsx
@@ -4,7 +4,7 @@ import styles from "./MediaList.module.css";
 export default function MediaList({ data }) {
   const dataRender = data.map((item, i) => (
     <li key={i} className={styles.MediaListItem}>
-      <a href={item.Link} target='_blank'>{item.Name}</a>
+      <a href={item.Link} target='_blank' rel="noreferrer">{item.Name}</a>
     </li>
   ));
   return <ul className={styles.MediaList}>{dataRender}</ul>;

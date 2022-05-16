93dba4f55235e61f2dbeaca5fcf268a09a38bcd4
Serge <sergesoroka@gmail.com>
2022-05-16 15:57:40 +0300

    Fixed Menu and  Pages Updates

--- components/FakeList/FakeList.jsx
+++ components/FakeList/FakeList.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import FakeItem from "./FakeItem/FakeItem";
 import styles from "./FakeList.module.css";
 
 export default function FakeList({ data }) {
-  const router = useRouter();
-
-  const pathnameRoute =
-    (router.pathname == "/" && "/narratives/") ||
-    (router.pathname == "/de" && "/de/narratives/") ||
-    (router.pathname == "/it" && "/it/narratives/") ||
-    (router.pathname == "/ru" && "/ru/narratives/") ||
-    (router.pathname == "/uk" && "/uk/narratives/");
-
   const unique = [...new Set(data.map((item) => item.Narrative))];
 
   const narrative = unique.map((n, i) => {
     return (
-      <Link key={i} href={{ pathname: `${pathnameRoute}${n}`, query: data }}>
+      <Link key={i} href={{ pathname: `/narratives/${n}`, query: data }}>
         <a>
           <FakeItem data={data} narrative={n} />
         </a>
--- pages/it/test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ItTest() {
-  return (
-    <div>IT Test</div>
-  )
-}
